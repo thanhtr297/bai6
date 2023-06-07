58e2e130ae07ba60fd66d3b22e12801ce49b3797
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2023-06-07 16:57:31 +0700

    adâd

---
+++ baitap sudung toan tu/toantu.js
@@ -0,0 +1,10 @@
+let inputWidth;
+let inputHeight;
+inputHeight = prompt('Enter the Height');
+inputWidth = prompt(' Enter the Width');
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+
+document.write('the area is: ' + area)
